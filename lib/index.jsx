0bbbc06aa77b1f12aa6c31a1c34ae03efb7bdc6b
xLsDg <xlsdg@qq.com>
2016-12-07 16:23:34 +0800

    feat(mod): init source files

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SimpleMDE from 'simplemde';
-import _isEqual from 'lodash.isequal';
+import _ from 'lodash';
 
 
 import 'simplemde/src/css/simplemde.css';
@@ -12,12 +12,24 @@ class ISimpleMDE extends React.Component {
     // console.log('constructor', props);
     super(props);
     this.state = {
+      instance: null
     };
     this._init = this._init.bind(this);
   }
   _init() {
     const that = this;
     // console.log('_init');
+    if (!that.state.instance) {
+      const dom = ReactDOM.findDOMNode(that);
+      const instance = new SimpleMDE({
+        element: dom
+      });
+      instance.value(that.props.text);
+      that.setState({
+        instance: instance
+      });
+      that.props.onReady(instance);
+    }
   }
   _update() {
     const that = this;
@@ -39,7 +51,7 @@ class ISimpleMDE extends React.Component {
   shouldComponentUpdate(nextProps, nextState) {
     const that = this;
     // console.log('shouldComponentUpdate', that.props, nextProps, that.state, nextState);
-    return (!_isEqual(nextProps.option, that.props.option));
+    return (!_.isEqual(nextProps.option, that.props.option));
   }
   componentWillUpdate(nextProps, nextState) {
     const that = this;
@@ -67,14 +79,16 @@ ISimpleMDE.propTypes = {
   style: React.PropTypes.object,
   option: React.PropTypes.object.isRequired,
   onReady: React.PropTypes.func,
-  loading: React.PropTypes.bool
+  loading: React.PropTypes.bool,
+  text: React.PropTypes.string
 };
 
 ISimpleMDE.defaultProps = {
   className: 'react-echarts',
   style: { width: '100%', height: '100%' },
   onReady: function(instance) {},
-  loading: false
+  loading: false,
+  text: ''
 };
 
 
